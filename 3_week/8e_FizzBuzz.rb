f3a1ca0b7861903fbc96fcebfa56cc1a9161ee1f
kangkyu <kangkyu1111@gmail.com>
2015-08-26 16:45:50 -0700

    refactor with method_name 3-e8-fizzbuzz
    put the code into a method taking argument for limit number
    to return a string when you call the method

--- 3_week/8e_FizzBuzz.rb
+++ 3_week/8e_FizzBuzz.rb
@@ -1,8 +1,11 @@
-b = (1..100).reduce("") do |output, num|
-      a = "#{'Fizz' if num % 3 == 0}" +
-          "#{'Buzz' if num % 5 == 0}"
-      a = num.to_s if a.empty?
-      output + a + "\n"
-    end
-puts b
+def fizz_buzz_to(limit_number)
+  (1..limit_number).reduce("") do |b, num|
+    a = "#{'Fizz' if num % 3 == 0}" +
+        "#{'Buzz' if num % 5 == 0}"
+    a = num.to_s if a.empty?
+    b + a + "\n"
+  end
+end
 
+# puts fizz_buzz_to(100)
+p fizz_buzz_to(100)
