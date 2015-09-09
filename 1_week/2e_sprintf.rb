a3d4e1ee2ad7d490e903444b89b5b76560fe4d1e
kangkyu <kangkyu1111@gmail.com>
2015-09-08 23:45:24 -0700

    week 1 2e add notes for sprintf

--- 1_week/2e_sprintf.rb
+++ 1_week/2e_sprintf.rb
@@ -11,3 +11,15 @@ figure out the output being printed by of this Ruby code.
 puts "%05d" % 123
 # 00123
 # => nil
+
+=begin
+
+http://ruby-doc.org/core-2.2.0/String.html#method-i-25
+http://ruby-doc.org/core-2.2.0/Kernel.html#method-i-sprintf
+
+%[flags][width][.precision]type
+width 4
+pad with 0 (flag)
+decimal number (d type)
+
+=end
