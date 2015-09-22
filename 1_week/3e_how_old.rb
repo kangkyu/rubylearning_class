ad2fad0763a60bb0aacec0a799c13c302709f80f
kangkyu <kangkyu1111@gmail.com>
2015-09-22 13:39:02 -0700

    1_week/3e_how_old

--- 1_week/3e_how_old.rb
+++ 1_week/3e_how_old.rb
@@ -2,4 +2,9 @@
 # Write a Ruby program that displays how old I am, if I am 979000000 seconds old. Display the result as a floating point (decimal) number to two decimal places (for example, 17.23).
 # Note: To format the output to say 2 decimal places, we can use the Kernel's format method. 
 x = 45.5678 
-format("%.2f", x) # will return the string 45.57
+p format("%.2f", x) # will return the string 45.57
+
+old_sec = 979_000_000
+sec_to_year = 60 * 60 * 24 * 365
+p x = old_sec.to_f / sec_to_year
+p format("%.2f", x) # returns "31.04"
